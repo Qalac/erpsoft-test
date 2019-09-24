37cca1bb108424f7ab1636ab90140d4396637d84
qalac <officialqalac@gmail.com>
2019-09-24 02:11:30 -0700

    final version of test

--- erpTest/settings.py
+++ erpTest/settings.py
@@ -119,3 +119,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+
+
+
+
+
+
+
--- register/views.py
+++ register/views.py
@@ -10,12 +10,12 @@ def list_object(request):
     query = Human.objects.all()
     return render(request, 'list.html', {'query': query})
 
-def create_object(request):
+def create_object(request):    
     if request.method=='POST':
         form = HumanForm(request.POST)
         if form.is_valid():
             form.save(commit=True)
-            return HttpResponse('SUCCESS')
+            return HttpResponse('SUCCESSFULLY SAVED INFO')
     else:
         form = HumanForm()
     return render(request,'form.html', {'form': form})
